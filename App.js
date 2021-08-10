25742fcef9a650af34dab070bc671608656ce63d
Arnab Bhattacharya <arnab@felicity.care>
2021-08-11 00:48:10 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import Home from './screens/home';
+import { StyleSheet, View, Text } from 'react-native';
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  console.log('Refreshed');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+}
---
+++ screens/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Home Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    },
+    title: {
+        fontFamily: 'nunito-bold',
+        fontSize: 20
+    }
+})
